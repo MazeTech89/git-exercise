5fcc8bbb0d6587e88a8ae55eb597d8b94f54eaf1
Moses Ademokun <mazetechsolution@gmail.com>
2026-01-31 13:17:51 +0000

    Adding script.sh which helps to create and add folder and files

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create folders
+mkdir fol_1
+mkdir fol_2
+
+# Create files in fol_1
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+
+# Create files in fol_2
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
+
+# Create .gitignore to ignore fol_1 and fol_2
+echo "fol_1" > .gitignore
+echo "fol_2" >> .gitignore
+
