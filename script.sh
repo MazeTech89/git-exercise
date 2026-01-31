00e033b7ce191a25c97c1e68c1efcfaf2a9239ae
MazeTech89 <74939600+MazeTech89@users.noreply.github.com>
2026-01-31 13:53:10 +0000

    Update script.sh
    After roll back  changes, I have re-entered the changes on github directly.
    
    Below was the roll back details:
    
    bad3342 (HEAD -> main, origin/main) Updating the README.md file
    abfaef6 Updating the bash script to delete fol_1, fol_2, fol_2 when it starts
    5fcc8bb Adding script.sh which helps to create and add folder and files
    90c78ba (feature-update) Added feature.txt
    80bb5db Add README file

--- script.sh
+++ script.sh
@@ -17,7 +17,27 @@ touch fol_2/2_1.txt
 touch fol_2/2_2.txt
 touch fol_2/2_3.txt
 
+# Set permissions: _1.txt and _3.txt -> read/write for user only
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+
+# Set permissions: _1.txt and _3.txt -> read/write for user only
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+
+# Set permissions: _2.txt -> read/write/execute for all users
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_2/2_2.txt
+
 # Create .gitignore to ignore fol_1 and fol_2
 echo "fol_1" > .gitignore
 echo "fol_2" >> .gitignore
 
+# Print completion message
+echo "Job completed"
